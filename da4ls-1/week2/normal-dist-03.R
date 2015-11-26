d0b7214303aa14ccdfe44151f8f8f4aaad104820
jxprat <jxprat@jxprat.uic.es>
2015-11-26 12:05:28 +0100

    Finishing exercise round 3 ...

--- da4ls-1/week2/normal-dist-03.R
+++ da4ls-1/week2/normal-dist-03.R
@@ -26,5 +26,57 @@ pnorm(3) - pnorm(-3)
 # from the average weight (remember to use popsd for the population sd)?
 library(dplyr)
 library(rafalib)
-y <- filter(dat,Sex=="M") %>% filter(Diet=="chow") %>% select(Bodyweight) %>% unlist
+## y <- filter(dat,Sex=="M") %>% filter(Diet=="chow") %>% select(Bodyweight) %>% unlist
+y <- filter(dat, Sex=="M" & Diet=="chow") %>% select(Bodyweight) %>% unlist
+z <- ( y - mean(y) ) / popsd(y)
+mean( abs(z) <=1 )
 
+# Exercise 5
+# What proportion of these numbers are within two 
+# standard deviations away from the list's average?
+mean(abs(z) <= 2)
+
+# Exercise 6
+# What proportion of these numbers are within three 
+# standard deviations away from the list's average?
+mean(abs(z) <= 3)
+
+# Exercise 7
+# Which of the following best describes the qq-plot 
+# comparing mouse weights to the normal distribution?
+qqnorm(z)
+abline(0,1)
+
+# Exercise 8
+# Create the above qq-plot for the four populations: male/females 
+# on each of the two diets. What is the most likely explanation for 
+# the mouse weights being well approximated? What is the best 
+# explanation for all these being well approximated by the 
+# normal distribution?
+mypar(2,2)
+y <- filter(dat, Sex=="M" & Diet=="chow") %>% select(Bodyweight) %>% unlist
+z <- ( y - mean(y) ) / popsd(y)
+qqnorm(z);abline(0,1)
+y <- filter(dat, Sex=="F" & Diet=="chow") %>% select(Bodyweight) %>% unlist
+z <- ( y - mean(y) ) / popsd(y)
+qqnorm(z);abline(0,1)
+y <- filter(dat, Sex=="M" & Diet=="hf") %>% select(Bodyweight) %>% unlist
+z <- ( y - mean(y) ) / popsd(y)
+qqnorm(z);abline(0,1)
+y <- filter(dat, Sex=="F" & Diet=="hf") %>% select(Bodyweight) %>% unlist
+z <- ( y - mean(y) ) / popsd(y)
+qqnorm(z);abline(0,1)
+
+# Exercise 9 
+# What is the average of the distribution of the sample average?
+y <- filter(dat, Sex=="M" & Diet=="chow") %>% select(Bodyweight) %>% unlist
+avgs <- replicate(10000, mean( sample(y, 25)))
+mypar(1,2)
+hist(avgs)
+qqnorm(avgs)
+qqline(avgs)
+mean(avgs)
+
+# Exercise 10
+# What is the standard deviation of the distribution of sample averages?
+sd(avgs)
