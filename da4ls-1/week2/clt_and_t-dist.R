cfe96dba643dbeead178500fe3d1f3ebd8dd807c
jxprat <jxprat@gmail.com>
2015-11-29 08:26:00 +0100

    Exercise of CTL ant t-distribution ...

---
+++ da4ls-1/week2/clt_and_t-dist.R
@@ -0,0 +1,7 @@
+# CTL and t-distribution in practice Exercises
+
+library(downloader)
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleMiceWeights.csv"
+filename <- "femaleMiceWeights.csv"
+if(!file.exists("femaleMiceWeights.csv")) download(url,destfile=filename)
+dat <- read.csv(filename)
